9a8499ba9ce1a3d94997d47ab1699da97e92657e
DucTran2310 <18520021@gm.uit.edu.vn>
2021-11-02 23:12:00 +0700

    Design trello basic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,117 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
     return (
-        <div className='App'>
-            <h1>Hello World!</h1>
+        <div className="trello-container">
+            <nav className="navbar app">App Bar</nav>
+            <nav className="navbar board">Board Bar</nav>
+            <div className="board-columns">
+                <div className="column">
+                    <header>Brainstore</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240?b=1&k=20&m=1295387240&s=170667a&w=0&h=KtWYFjSBgpNgVkE3P-WKZ2F6V-VxyUBBtJIP_k8IANM="
+                                alt="img"
+                            />
+                            Title: ADSTAR
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstore</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240?b=1&k=20&m=1295387240&s=170667a&w=0&h=KtWYFjSBgpNgVkE3P-WKZ2F6V-VxyUBBtJIP_k8IANM="
+                                alt="img"
+                            />
+                            Title: ADSTAR
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstore</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240?b=1&k=20&m=1295387240&s=170667a&w=0&h=KtWYFjSBgpNgVkE3P-WKZ2F6V-VxyUBBtJIP_k8IANM="
+                                alt="img"
+                            />
+                            Title: ADSTAR
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstore</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240?b=1&k=20&m=1295387240&s=170667a&w=0&h=KtWYFjSBgpNgVkE3P-WKZ2F6V-VxyUBBtJIP_k8IANM="
+                                alt="img"
+                            />
+                            Title: ADSTAR
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstore</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240?b=1&k=20&m=1295387240&s=170667a&w=0&h=KtWYFjSBgpNgVkE3P-WKZ2F6V-VxyUBBtJIP_k8IANM="
+                                alt="img"
+                            />
+                            Title: ADSTAR
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+            </div>
         </div>
     );
 }
