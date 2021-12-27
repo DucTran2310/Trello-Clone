317f54f72feddb486ce32448e0b2095ec303c172
DucTran2310 <18520021@gm.uit.edu.vn>
2021-12-27 10:58:53 +0700

    Deploy

--- src/untilities/constants.js
+++ src/untilities/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://localhost:8080'
-
+// export const API_ROOT = 'http://localhost:8080'
+export const API_ROOT = 'https://trello-clone-adstar-api.herokuapp.com'
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
